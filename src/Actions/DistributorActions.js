cae865beaab02cc9f338b74da7c9d5523f666874
Adam <adt6261@gmail.com>
2018-08-07 18:44:57 -0400

    stubbed out actions and reducers relating to distributors

--- src/Actions/DistributorActions.js
+++ src/Actions/DistributorActions.js
@@ -2,59 +2,51 @@ import fetch from 'isomorphic-fetch';
 import { history } from '../App'
 
 export function CreateDistributor(distributor){ 
-  // return function(dispatch, getState){ 
-  //   dispatch({type: 'ADD_PERSON'})
-  //   return fetch('https://rateyouracquaintanceapi.herokuapp.com/people', {
-  //   method: 'POST',
-  //   headers: { 
-  //     Authorization: `Bearer ${localStorage.getItem("token")}`,
-  //     Accept: 'application/json',
-  //     'Content-Type': 'application/json'
-  //   },
-  //   body: JSON.stringify(person)})
-  //   .then(res => {
-  //     return res.json()
-  //   }).then(responseJson => {
-  //     dispatch({type: 'SET_PERSON', payload: responseJson})     
-  //   }).then(res => { 
-  //     let currentPerson = getState().peopleReducer.person
-  //      if (currentPerson.id || currentPerson === "unfound") {
-  //       history.push(`/people/${currentPerson.id}`)  
-  //      } 
-  //      else { 
-  //       history.push(`/people/`)
-  //      } 
-  //   })
-  // } 
+  return function(dispatch, getState){ 
+    dispatch({type: 'CREATE_DISTRIBUTOR'})
+    return fetch('https://fbaprofprodsapi.herokuapp.com/distributors', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(distributor)})
+    .then(res => {
+      return res.json()
+    }).then(responseJson => {
+      dispatch({type: 'SET_DISTRIBUTOR', payload: responseJson})     
+    }).then(res => { 
+      let currentDistributor = getState().DistributorsReducer.distributor
+      history.push(`/distributors/${currentDistributor.id}`)  
+    })
+  } 
 }  
 
 export function UploadFile(sheet) { 
-  //Triggers post to route where file is uploaded to backend. This request completes, then triggers request to amazon sellers API for each JSONified item in list. This request completes and sends back prof_prods, triggering request to save each prof prod. User is then redirected to distributors show page with new prof prods added"
+  //Triggers post to route where file is uploaded to backend. This request completes, then triggers request to amazon sellers API for each JSONified item in list. This request completes and sends back prof_prods, triggering request to save each prof prod. User is then redirected to distributors show page with new prof prods added
+  return function(dispatch, getState){ 
+    let currentDistributor = getState().DistributorsReducer.distributor
+    dispatch({type: 'UPLOAD_AND_PARSE'})
+    return fetch(`https://fbaprofprodsapi.herokuapp.com/distributors/${currentDistributor.id}/upload`, {
+    method: 'POST',
+    headers: {
+      Accept: 'multipart/form-data',
+      'Content-Type': 'multipart/form-data'
+    },
+    body: sheet})
+    .then(res => {
+      return res.json()
+    }).then(responseJson => {
+      dispatch({type: 'CHECK_PROFITABILITY', payload: responseJson}) 
+      //somehow backend route needs to take json data parsed from file and check each product against Amazon Seller API to see if it is profitable   
+    }).then(res => { 
+      history.push(`/distributors/${currentDistributor.id}`)  
+    })
+  } 
 }
 
-// export function addReview(review, person){ 
-//   return function(dispatch, getState){
-//     dispatch({type: 'ADD_REVIEW'})
-//     return fetch(`https://rateyouracquaintanceapi.herokuapp.com/people/${person.id}/reviews`, {
-//     method: 'POST',
-//     headers: {
-//       Authorization: `Bearer ${localStorage.getItem("token")}`,
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(review)}) 
-//     .then(res => {
-//       return res.json()
-//     }).then(responseJson => {
-//       dispatch({type: 'CONCAT_REVIEW', payload: responseJson}) 
-//     }).then(res => { 
-//       history.push(`/people/${getState().peopleReducer.person.id}`) 
-//     })  
-//   } 
-// }   
-
-// export function removePerson(){ 
-//   return function(dispatch){
-//    dispatch({type: 'CLEAR_PERSON'})
-//  } 
-// } 
+export function removeDistributor(){ 
+  return function(dispatch){
+   dispatch({type: 'CLEAR_DISTRIBUTOR'})
+ } 
+} 
--- src/Reducers/distributors_reducer.js
+++ src/Reducers/distributors_reducer.js
@@ -1,31 +1,14 @@
-export function DistributorsReducer(state = {person: "unfound", people: {}
+export function DistributorsReducer(state = {distributor: null, distributors: {}
 }, action) {
   switch (action.type) {
-
-      // case 'GET_PERSON': 
-      //   let currentPerson = action.payload[0].filter((person => person.name == action.payload[1].name)) 
-      //   if (currentPerson != undefined && currentPerson.length === 1) { 
-      //     return {person: currentPerson[0], people: state.people}; 
-      //   } else if (currentPerson.length > 1) { 
-      //     return {person: currentPerson, people: currentPerson};
-      //   } else { 
-      //     return  {person: state.person, people: state.people}; 
-      //   }
-      // case 'SET_PERSON':  
-      //   currentPerson = action.payload;
-      //   return {person: currentPerson}; 
-      // case 'CONCAT_REVIEW': 
-      //   let review = action.payload 
-      //   let reviews = state.person.reviews.concat(review) 
-      //   state.person.reviews = reviews
-      //   return {person: state.person, people: state.people}; 
-      // case 'CLEAR_PERSON': 
-      //   return {person: "unfound", people: {}} 
-      // case 'SET_PERSON_BY_ID': 
-      //   currentPerson = action.payload[0].filter((person => person.id == action.payload[1]));
-      //   return {person: currentPerson[0], people: state.people};
+      case 'SET_DISTRIBUTOR':  
+        let currentDistributor = action.payload;
+        return {distributor: currentDistributor}; 
+      case 'UPLOAD_AND_PARSE': 
+        return {distributor: currentDistributor};
+      case 'CLEAR_DISTRIBUTOR': 
+        return {distributor: null, distributors: {}} 
       default:
-      //  return state; 
-      return null;
+       return state; 
   }
 } 
