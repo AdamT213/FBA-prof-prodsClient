9dff2db4ee652f3c0b8ef310deaaad4145be3aa4
Adam <adt6261@gmail.com>
2018-08-07 14:01:37 -0400

    adding components related to index, show, and new pages for distributors

---
+++ src/Components/DistributorsList.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Distributor from '../Presentational/Distributor';
+import AddDistributor from './Forms/AddDistributor'
+import Distributors from './Distributors'
+
+class DistributorsList extends Component {
+
+  render() {
+
+    const distributors = this.props.distributors.map((distributor, index) => {
+      return <Distributor name={distributor.name} key={index} />
+    });
+
+    return (
+        <div className= "App"> 
+        <Router> 
+        <div>
+        <ul>
+          <li><Link to={`${this.props.match.url}/new`}>
+              Add a New Distributor
+            </Link></li>
+          </ul>
+          <div>
+            <h3>Here are all of your Current Distributors</h3>
+          </div>
+          <ul>
+            {distributors}
+           </ul>
+          <Switch>
+            <Route path={`${this.props.match.url}/new`} component={AddDistributor} />
+          </Switch> 
+          </div>
+          </Router>
+        </div>
+    )
+  }
+};
+
+function mapStateToProps(state){ 
+  return {distributors: state.DistributorsReducer.distributors}
+}
+
+export default connect(mapStateToProps)(DistributorsList);
---
+++ src/Forms/AddDistributor.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'; 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+import { CreateDistributor } from '../Actions/DistributorActions'; 
+import { Distributor } from '../Components/Distributor'
+
+
+export class AddDistributor extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = { 
+      name: '',
+    }
+  }
+
+  handleOnChange = event => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleOnSubmit = event => { 
+    const distributor = Object.assign({}, this.state); 
+    event.preventDefault();
+    this.props.AddDistributor(distributor);   
+    this.setState({
+      name: '', 
+    });
+  }
+
+  render() { 
+
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2">
+              <div className="panel panel-default">
+                <div className="panel-body">
+                  <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="name" className="col-md-4 control-label">Distributor's Name</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          name="name"
+                          value={this.state.name}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-md-6 col-md-offset-4">
+                        <button type="submit" className="btn btn-default">Add Distributor</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+      ); 
+    }
+  }
+export default connect(null, { AddDistributor })(CreateDistributor);
